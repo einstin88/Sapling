99ea49b66c70cd6d70b4136a8e44f76a931f0606
einstin88 <62468100+einstin88@users.noreply.github.com>
2020-09-18 19:54:31 +0200

    fix issue with pyinstaller commands

--- compile/pyinstall_withUPX.bat
+++
@@ -1,6 +0,0 @@
-cd C:\Users\pelie\venv\Scripts
-pyinstaller --clean ^
---upx-dir C:\Users\pelie\anaconda3\upx-3.96-win64 ^
---distpath="C:\Users\pelie\Documents\eDx\Python\Project Sapling\dist" ^
---workpath="C:\Users\pelie\Documents\eDx\Python\Project Sapling\tmp" ^
-"C:\Users\pelie\Documents\eDx\Python\Project Sapling\Sapling.spec"
